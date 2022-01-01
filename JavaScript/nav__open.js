9390e8b9fe25e604390ad721fab0ab36889b9d99
0sergej <sergej2stojanovic@gmail.com>
2022-01-01 21:58:09 +0100

    mobile test done

--- JavaScript/nav__open.js
+++ JavaScript/nav__open.js
@@ -1,4 +1,7 @@
 const navBtn = document.querySelector('.nav__open');
+const heroBtnRight = document.querySelector('.hero__btn--right');
+const heroBtnLeft = document.querySelector('.hero__btn--left')
+
 let navOpen = false;
 
 navBtn.addEventListener('click', () =>
@@ -7,12 +10,16 @@ navBtn.addEventListener('click', () =>
     {
         navBtn.classList.add('close');
         navBtn.classList.remove('open');
+        heroBtnLeft.classList.remove('disabled');
+        heroBtnRight.classList.remove('disabled');
         navOpen = true;
     }
     else
     {
         navBtn.classList.remove('close');
         navBtn.classList.add('open');
-        navOpen = false;
+        heroBtnLeft.classList.add('disabled');
+        heroBtnRight.classList.add('disabled');
+        navOpen = false
     }
 });
